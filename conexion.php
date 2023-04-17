9ca7581ff68c5a04690fa47b85f5f7b21f1971ab
AnaYelitza <ana.ibanez@tecsup.edu.pe>
2023-04-16 19:28:43 -0500

    mi primer commit ana

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	$host = "host=containers-us-west-169.railway.app";
+	$port = "port=7272";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=iIOQ1Azcw9FHktHkiXzn";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
